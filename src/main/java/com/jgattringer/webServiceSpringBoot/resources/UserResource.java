0199bc194dffb1594ad893a901377afd8dbcaf57
JGattringer <joaogattringer@hotmail.com>
2024-05-28 13:19:16 -0300

    add service layer, component registration

--- src/main/java/com/jgattringer/webServiceSpringBoot/resources/UserResource.java
+++ src/main/java/com/jgattringer/webServiceSpringBoot/resources/UserResource.java
@@ -1,18 +1,34 @@
 package com.jgattringer.webServiceSpringBoot.resources;
 
 import com.jgattringer.webServiceSpringBoot.entities.User;
+import com.jgattringer.webServiceSpringBoot.services.UserServices;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserServices service;
+
     @GetMapping
-    public ResponseEntity<User> findAll() {
-        User u = new User(1L, "maria", "maria@gmail.com", "322425689", "123345" );
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> findAll() {
+        List<User> list = service.findAll();
+
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        User obj = service.findByI(id);
+
+        return ResponseEntity.ok().body(obj);
     }
 }
