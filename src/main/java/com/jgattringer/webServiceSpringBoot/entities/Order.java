d3f3cef8cbae2fe6568c0f6b85edaaee2ae44afe
JGattringer <joaogattringer@hotmail.com>
2024-05-29 12:38:48 -0300

    add enum OrderStatus

--- src/main/java/com/jgattringer/webServiceSpringBoot/entities/Order.java
+++ src/main/java/com/jgattringer/webServiceSpringBoot/entities/Order.java
@@ -1,6 +1,7 @@
 package com.jgattringer.webServiceSpringBoot.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.jgattringer.webServiceSpringBoot.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -25,6 +26,8 @@ public class Order implements Serializable {
    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+   private Integer orderStatus;
+
     // Defines a many-to-one relationship with the User entity
     @ManyToOne
     // Specifies the foreign key column name in the database table
@@ -35,9 +38,10 @@ public class Order implements Serializable {
     public Order() {}
 
     // Constructor with all fields
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -56,6 +60,16 @@ public class Order implements Serializable {
 
     public void setMoment(Instant moment) {
         this.moment = moment;
+    }
+
+    public OrderStatus getOrderStatus(){
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus){
+        if(orderStatus  != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     public User getClient() {
