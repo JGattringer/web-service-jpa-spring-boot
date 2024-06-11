5e58caa73bf84626270f0e3db22028479a7a17b5
JGattringer <joaogattringer@hotmail.com>
2024-06-11 10:48:12 -0300

    add update error handling

--- src/main/java/com/jgattringer/webServiceSpringBoot/services/UserServices.java
+++ src/main/java/com/jgattringer/webServiceSpringBoot/services/UserServices.java
@@ -4,6 +4,7 @@ import com.jgattringer.webServiceSpringBoot.entities.User;
 import com.jgattringer.webServiceSpringBoot.repositories.UserRepository;
 import com.jgattringer.webServiceSpringBoot.services.exceptions.DataBaseException;
 import com.jgattringer.webServiceSpringBoot.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -44,9 +45,14 @@ public class UserServices {
     }
 
     public User update(Long id, User obj){
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try{
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
+
     }
 
     private void updateData(User entity, User obj) {
