44e913bc532a64d406197565d2ab0f97ea5aefb8
JGattringer <joaogattringer@hotmail.com>
2024-06-11 10:40:57 -0300

    add delete exception

--- src/main/java/com/jgattringer/webServiceSpringBoot/services/UserServices.java
+++ src/main/java/com/jgattringer/webServiceSpringBoot/services/UserServices.java
@@ -2,8 +2,11 @@ package com.jgattringer.webServiceSpringBoot.services;
 
 import com.jgattringer.webServiceSpringBoot.entities.User;
 import com.jgattringer.webServiceSpringBoot.repositories.UserRepository;
+import com.jgattringer.webServiceSpringBoot.services.exceptions.DataBaseException;
 import com.jgattringer.webServiceSpringBoot.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -29,7 +32,15 @@ public class UserServices {
     }
 
     public void delete(Long id) {
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        }
+        catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        }
+        catch (DataIntegrityViolationException e) {
+            throw new DataBaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User obj){
