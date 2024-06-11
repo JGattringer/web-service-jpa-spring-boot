db75e414b6733f92114599100c6958846d85c08a
JGattringer <joaogattringer@hotmail.com>
2024-06-11 09:39:29 -0300

    add delete user

--- src/main/java/com/jgattringer/webServiceSpringBoot/resources/UserResource.java
+++ src/main/java/com/jgattringer/webServiceSpringBoot/resources/UserResource.java
@@ -31,10 +31,16 @@ public class UserResource {
         return ResponseEntity.ok().body(obj);
     }
 
-    @PostMapping()
+    @PostMapping
     public ResponseEntity<User> insert(@RequestBody User obj){
         obj = service.insert(obj);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
         return ResponseEntity.created(uri).body(obj);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public  ResponseEntity<Void> delete(@PathVariable Long id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
